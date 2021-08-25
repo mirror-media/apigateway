186c1049946d19fbe0ae8c346640bf8191ac9cc0
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-08-25 12:28:04 +0800

    feat(handler): separate the schema and merge it in the handler to help maintaince

--- handler/apigateway.go
+++ handler/apigateway.go
@@ -3,10 +3,10 @@ package handler
 import (
 	"context"
 	"net/http"
-	"os"
 	"time"
 
 	http2 "github.com/jensneuse/graphql-go-tools/examples/federation/gateway/http"
+	"github.com/mirror-media/mm-apigateway/graph"
 
 	"github.com/jensneuse/abstractlogger"
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/datasource/graphql_datasource"
@@ -18,24 +18,34 @@ import (
 
 var logger = abstractlogger.NewLogrusLogger(logrus.New(), abstractlogger.InfoLevel)
 
-func NewAPIGatewayGraphQLHandler(memberUpstreamURL, schemaPath string) http.Handler {
-	schemaReader, err := os.Open(schemaPath)
+func NewAPIGatewayGraphQLHandler(memberUpstreamURL, mutationUpstreamURL, typeSchemaPath, querySchemaPath, mutationSchemaPath string) http.Handler {
+
+	querySchema, err := graph.AlchemizeSchema(typeSchemaPath, querySchemaPath)
 	if err != nil {
 		logrus.Panic(err)
 	}
 
-	schema, err := graphql.NewSchemaFromReader(schemaReader)
-	schemaReader.Close()
-	if err != nil {
-		logrus.Panic(err)
-	}
-	validation, err := schema.Validate()
+	validation, err := querySchema.Validate()
 	if err != nil {
 		logrus.Panic(err)
 	}
 	if !validation.Valid {
 		validation.Errors.ErrorByIndex(0)
-		logrus.Panic("schema is not valid:", validation.Errors.Error(), "first one is:", validation.Errors.ErrorByIndex(0))
+		logrus.Panic("query schema is not valid:", validation.Errors.Error(), "first one is:", validation.Errors.ErrorByIndex(0))
+	}
+
+	mutationSchema, err := graph.AlchemizeSchema(typeSchemaPath, mutationSchemaPath)
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	validation, err = mutationSchema.Validate()
+	if err != nil {
+		logrus.Panic(err)
+	}
+	if !validation.Valid {
+		validation.Errors.ErrorByIndex(0)
+		logrus.Panic("mutation schema is not valid:", validation.Errors.Error(), "first one is:", validation.Errors.ErrorByIndex(0))
 	}
 
 	defaultClient := httpclient.DefaultNetHttpClient
@@ -50,15 +60,25 @@ func NewAPIGatewayGraphQLHandler(memberUpstreamURL, schemaPath string) http.Hand
 			Jar:           defaultClient.Jar,
 			Timeout:       defaultClient.Timeout,
 		},
-		graphql_datasource.Configuration{
-			Fetch: graphql_datasource.FetchConfiguration{
-				URL: memberUpstreamURL,
+		[]graphql_datasource.Configuration{
+			{
+				Fetch: graphql_datasource.FetchConfiguration{
+					URL: memberUpstreamURL,
+				},
+				Federation: graphql_datasource.FederationConfiguration{
+					Enabled:    false,
+					ServiceSDL: string(querySchema.Document()),
+				},
+			}, {
+				Fetch: graphql_datasource.FetchConfiguration{
+					URL: mutationUpstreamURL,
+				},
+				Federation: graphql_datasource.FederationConfiguration{
+					Enabled:    false,
+					ServiceSDL: string(mutationSchema.Document()),
+				},
 			},
-			Federation: graphql_datasource.FederationConfiguration{
-				Enabled:    false,
-				ServiceSDL: string(schema.Document()),
-			},
-		},
+		}...,
 	)
 
 	engineConfig, err := factory.EngineV2Configuration()
@@ -68,8 +88,13 @@ func NewAPIGatewayGraphQLHandler(memberUpstreamURL, schemaPath string) http.Hand
 
 	executionEngine, _ := graphql.NewExecutionEngineV2(context.TODO(), logger, engineConfig)
 
+	mergedSchema, err := factory.MergedSchema()
+	if err != nil {
+		logrus.Panic(err)
+	}
+
 	// TODO extract the handler wrapper from the example
-	handler := http2.NewGraphqlHTTPHandler(schema, executionEngine, nil, logger)
+	handler := http2.NewGraphqlHTTPHandler(mergedSchema, executionEngine, nil, logger)
 
 	return handler
 }
