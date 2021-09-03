5f01c1b00b70706aa66e0cc7b60870a1c17025b1
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-09-04 00:50:53 +0800

    feat(payment): add CreateNewebpayAgreementPayload method to store

--- payment/newebpay.go
+++ payment/newebpay.go
@@ -74,3 +74,101 @@ func getCallbackUrl(protocol, domain, path string, purchaseInfo *PurchaseInfo) (
 	return fmt.Sprintf("%s://%s%s?%s", protocol, domain, path, v.Encode()), err
 }
 
+const unsafeCharacters = ":/?#[]@!$&'()*+,;=<>%{}|\\^\"`\n"
+
+type PostPurchased struct {
+	ID    string
+	Slug  string
+	Title string
+}
+
+type TradeInfo struct {
+	Amt                 int    `url:"Amt"`
+	ClientBackURL       string `url:"ClientBackURL,omitempty"`
+	Email               string `url:"Email"`
+	IsAbleToModifyEmail int8   `url:"EmailModify"`
+	LoginType           int8   `url:"LoginType"`
+	MerchantOrderNo     string `url:"MerchantOrderNo"`
+	NotifyURL           string `url:"NotifyURL,omitempty"`
+	RespondType         string `url:"RespondType,omitempty"`
+	ReturnURL           string `url:"ReturnURL,omitempty"`
+	StoreID             string `url:"MerchantID"`
+	TimeStamp           string `url:"TimeStamp"`
+	Version             string `url:"Version"`
+}
+
+type TradeInfoAgreement struct {
+	TradeInfo
+	CreditAgreement int8   `url:"CREDITAGREEMENT"` // Use 1
+	OrderComment    string `url:"OrderComment"`
+	P3D             int8   `url:"P3D"`
+	TokenTerm       string `url:"TokenTerm"`
+}
+
+type TradeInfoMGP struct {
+	TradeInfo
+	ItemDescription string `url:"ItemDesc,omitempty"`
+}
+
+// Ref: https://github.com/mirror-media/apigateway/files/6866871/NewebPay_._._AGREEMENT_.1.0.6.pdf
+func (s Store) CreateNewebpayAgreementPayload(firebaseID, tokenTerm string, subscription model.Subscription, purchaseInfo PurchaseInfo) (payload string, err error) {
+	// Validate the data at the beginning for short circuit
+	if subscription.CreatedAt == nil {
+		return "", fmt.Errorf("subscription(%s) has not creation time", subscription.ID)
+	} else if subscription.Amount == nil {
+		return "", fmt.Errorf("subscription(%s) has no amount", subscription.ID)
+	} else if subscription.OrderNumber == nil {
+		return "", fmt.Errorf("subscription(%s) has no OrderNumber", subscription.ID)
+	} else if subscription.Email == nil {
+		return "", fmt.Errorf("subscription(%s) has no email", subscription.ID)
+	} else if subscription.Desc == nil {
+		return "", fmt.Errorf("subscription(%s) has no descrption", subscription.ID)
+	} else if i := strings.IndexAny(*subscription.Desc, unsafeCharacters); i != -1 {
+		return "", fmt.Errorf("subscription(%s) description contains unsafe a character(%s)", subscription.ID, (*subscription.Desc)[i:i+1])
+	}
+
+	timestamp, err := time.Parse(time.RFC3339, *subscription.CreatedAt)
+	if err != nil {
+		return "", err
+	}
+
+	returnURL, err := s.getReturnURL(purchaseInfo)
+	if err != nil {
+		return "", nil
+	}
+
+	clientBackURL, err := s.getClientBackPath(purchaseInfo)
+	if err != nil {
+		return "", nil
+	}
+
+	notifyURL, err := s.getNotifyURL(purchaseInfo)
+	if err != nil {
+		return "", nil
+	}
+
+	tradeInfo := TradeInfoAgreement{
+		TradeInfo: TradeInfo{
+			Amt:                 *subscription.Amount,
+			ClientBackURL:       clientBackURL,
+			Email:               *subscription.Email,
+			IsAbleToModifyEmail: s.IsAbleToModifyEmail,
+			LoginType:           s.LoginType,
+			MerchantOrderNo:     *subscription.OrderNumber,
+			NotifyURL:           notifyURL,
+			RespondType:         "JSON",
+			ReturnURL:           returnURL,
+			StoreID:             s.ID,
+			TimeStamp:           strconv.FormatInt(timestamp.Unix(), 10),
+			Version:             s.Version,
+		},
+		CreditAgreement: 1,
+		// FIXME Are you sure?
+		OrderComment: *subscription.Desc,
+		P3D:          s.P3D,
+		TokenTerm:    tokenTerm,
+	}
+	v, err := query.Values(tradeInfo)
+	payload = v.Encode()
+	return payload, err
+}
--- payment/newebpay_test.go
+++ payment/newebpay_test.go
@@ -7,6 +7,129 @@ import (
 	"github.com/mirror-media/apigateway/graph/member/model"
 )
 
+func TestStore_CreateNewebpayAgreementPayload(t *testing.T) {
+	amount := 999
+	timeUnix := int64(1630665558)
+	createAt := time.Unix(timeUnix, 0).Format(time.RFC3339)
+	email := "aaa@bbb.cc"
+	orderNumber := "c4ovmad948155cflse1g"
+	description := "subscription description"
+	subscription := model.Subscription{
+		Amount:      &amount,
+		CreatedAt:   &createAt,
+		OrderNumber: &orderNumber,
+		Email:       &email,
+		Desc:        &description,
+	}
+	unsafeChar := "\n"
+	unsafeCharSubscription := subscription
+	unsafeCharSubscription.Desc = &unsafeChar
+	type fields struct {
+		ClientBackURL       string
+		CreditAgreement     int8
+		ID                  string
+		IsAbleToModifyEmail int8
+		LoginType           int8
+		NotifyURL           string
+		P3D                 int8
+		RespondType         string
+		ReturnURL           string
+		Version             string
+	}
+	store := fields{
+		ClientBackURL:       "http://ClientBackURL/1?a=b&c=d",
+		CreditAgreement:     1,
+		ID:                  "store id",
+		IsAbleToModifyEmail: 1,
+		LoginType:           0,
+		NotifyURL:           "http://NotifyURL/1?a=b&c=d",
+		P3D:                 1,
+		RespondType:         "JSON",
+		ReturnURL:           "http://ReturnURL",
+		Version:             "1.6",
+	}
+	type args struct {
+		firebaseID   string
+		tokenTerm    string
+		subscription model.Subscription
+		purchaseInfo PurchaseInfo
+	}
+	tests := []struct {
+		name        string
+		fields      fields
+		args        args
+		wantPayload string
+		wantErr     bool
+	}{
+		{
+			name:   "Successfull Recurring Case",
+			fields: store,
+			args: args{
+				tokenTerm:    "token_term",
+				subscription: subscription,
+				purchaseInfo: PurchaseInfo{
+					Merchandise: Merchandise{
+						Code:      "yearly",
+						PostID:    "postid",
+						PostSlug:  "postslug",
+						PostTitle: "posttitle",
+						Amount:    8888,
+					},
+					PurchasedAtUnixTime: 111,
+					OrderNumber:         "ordernumber",
+					MemberFirebaseID:    "memberid",
+					ReturnPath:          "/story/abc",
+				},
+			},
+			wantPayload: "Amt=999&CREDITAGREEMENT=1&ClientBackURL=%3A%2F%2F%2Fhttp%3A%2F%2FClientBackURL%2F1%3Fa%3Db%26c%3Dd%3Famount%3D8888%26code%3Dyearly%26memberFirebaseId%3Dmemberid%26orderNumber%3Dordernumber%26postId%3Dpostid%26postSlug%3Dpostslug%26postTitle%3Dposttitle%26purchasedAtUnixTime%3D111%26returnPath%3D%252Fstory%252Fabc&Email=aaa%40bbb.cc&EmailModify=1&LoginType=0&MerchantID=store+id&MerchantOrderNo=c4ovmad948155cflse1g&NotifyURL=%3A%2F%2F%2Fhttp%3A%2F%2FNotifyURL%2F1%3Fa%3Db%26c%3Dd&OrderComment=subscription+description&P3D=1&RespondType=JSON&ReturnURL=%3A%2F%2F%2Fhttp%3A%2F%2FReturnURL%3Famount%3D8888%26code%3Dyearly%26memberFirebaseId%3Dmemberid%26orderNumber%3Dordernumber%26postId%3Dpostid%26postSlug%3Dpostslug%26postTitle%3Dposttitle%26purchasedAtUnixTime%3D111%26returnPath%3D%252Fstory%252Fabc&TimeStamp=1630665558&TokenTerm=token_term&Version=1.6",
+			wantErr:     false,
+		},
+		{
+			name:   "unsafe char",
+			fields: store,
+			args: args{
+				tokenTerm:    "token_term",
+				subscription: unsafeCharSubscription,
+			},
+			wantPayload: "",
+			wantErr:     true,
+		},
+		{
+			name:   "invalid empty subscription",
+			fields: store,
+			args: args{
+				tokenTerm:    "token_term",
+				subscription: model.Subscription{},
+			},
+			wantPayload: "",
+			wantErr:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Store{
+				ClientBackPath:      tt.fields.ClientBackURL,
+				ID:                  tt.fields.ID,
+				IsAbleToModifyEmail: tt.fields.IsAbleToModifyEmail,
+				LoginType:           tt.fields.LoginType,
+				NotifyPath:          tt.fields.NotifyURL,
+				P3D:                 tt.fields.P3D,
+				RespondType:         tt.fields.RespondType,
+				ReturnPath:          tt.fields.ReturnURL,
+				Version:             tt.fields.Version,
+			}
+			gotPayload, err := s.CreateNewebpayAgreementPayload(tt.args.firebaseID, tt.args.tokenTerm, tt.args.subscription, tt.args.purchaseInfo)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Store.CreateNewebpayAgreementPayload() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotPayload != tt.wantPayload {
+				t.Errorf("Store.CreateNewebpayAgreementPayload() = %v, want %v", gotPayload, tt.wantPayload)
+			}
+		})
+	}
+}
+
 func TestStore_GetNotifyURL(t *testing.T) {
 	type fields struct {
 		CallbackDomain      string
@@ -41,7 +164,7 @@ func TestStore_GetNotifyURL(t *testing.T) {
 		{
 			name: "one-time",
 			args: args{
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:      "one_time",
 						PostID:    "postid",
@@ -61,7 +184,7 @@ func TestStore_GetNotifyURL(t *testing.T) {
 		{
 			name: "monthly",
 			args: args{
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:   "monthly",
 						Amount: 8888,
@@ -78,7 +201,7 @@ func TestStore_GetNotifyURL(t *testing.T) {
 		{
 			name: "yearly",
 			args: args{
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:   "yearly",
 						Amount: 8888,
@@ -156,7 +279,7 @@ func TestStore_GetReturnURL(t *testing.T) {
 		{
 			name: "one-time",
 			args: args{
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:      "one_time",
 						PostID:    "postid",
@@ -176,7 +299,7 @@ func TestStore_GetReturnURL(t *testing.T) {
 		{
 			name: "monthly",
 			args: args{
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:   "monthly",
 						Amount: 8888,
@@ -193,7 +316,7 @@ func TestStore_GetReturnURL(t *testing.T) {
 		{
 			name: "yearly",
 			args: args{
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:   "yearly",
 						Amount: 8888,
@@ -275,7 +398,7 @@ func TestStore_GetClientBackPath(t *testing.T) {
 			args: args{
 				"https",
 				"domain",
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:      "one_time",
 						PostID:    "postid",
@@ -297,7 +420,7 @@ func TestStore_GetClientBackPath(t *testing.T) {
 			args: args{
 				"https",
 				"domain",
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:   "monthly",
 						Amount: 8888,
@@ -316,7 +439,7 @@ func TestStore_GetClientBackPath(t *testing.T) {
 			args: args{
 				"https",
 				"domain",
-				CallbackInfo{
+				PurchaseInfo{
 					Merchandise: Merchandise{
 						Code:   "yearly",
 						Amount: 8888,
