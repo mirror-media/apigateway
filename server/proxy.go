900b98cacea8fa52db4aa955ba26cf7423564d9e
bcgodev <21715194+bcgodev@users.noreply.github.com>
2021-09-08 18:00:24 +0800

    fix(proxy): fix cache and truncate function

--- server/proxy.go
+++ server/proxy.go
@@ -50,7 +50,12 @@ func NewSingleHostReverseProxy(target *url.URL, pathBaseToStrip string, rdb cach
 		}
 	}
 	return func(c *gin.Context) {
-		// TODO refactor modification and cache code
+
+		logger := logrus.WithFields(logrus.Fields{
+			"path": c.FullPath(),
+		})
+
+		var err error
 		var tokenState string
 
 		tokenSaved, exist := c.Get(middleware.GCtxTokenKey)
@@ -60,47 +65,11 @@ func NewSingleHostReverseProxy(target *url.URL, pathBaseToStrip string, rdb cach
 			tokenState = tokenSaved.(token.Token).GetTokenState()
 		}
 
-		switch path := c.Request.URL.Path; {
-		case strings.HasSuffix(path, "/getposts") || strings.HasSuffix(path, "/posts") || strings.HasSuffix(path, "/post"):
-			// Try to read cache first
-			var key string
-			if tokenState != token.OK {
-				key = fmt.Sprintf("%s.%s.%s.%s", "apigateway", "post", "truncated", c.Request.RequestURI)
-			} else {
-				key = fmt.Sprintf("%s.%s.%s.%s", "apigateway", "post", "clean", c.Request.RequestURI)
-			}
-
-			cmd := rdb.Get(context.TODO(), key)
-			// cache doesn't exist, do fetch reverse proxy
-			if cmd == nil {
-				break
-			}
-			body, err := cmd.Bytes()
-			// cache can't be understood, do fetch reverse proxy
-			if err != nil {
-				break
-			}
-
-			c.AbortWithStatusJSON(http.StatusOK, Reply{
-				TokenState: tokenState,
-				Data:       json.RawMessage(body),
-			})
-			return
-		}
-
-		var subscribedPostIDs = make(chan map[string]interface{}, 1)
-		var premiumPrivilege = make(chan bool, 1)
-		var errChan = make(chan error, 1)
-		if tokenState != token.OK {
-			subscribedPostIDs <- nil
-			premiumPrivilege <- false
-			errChan <- nil
-		} else {
-			tokenState = tokenSaved.(token.Token).GetTokenState()
-			go func() {
-				if tokenState == token.OK {
-					firebaseID := c.GetString(middleware.GCtxUserIDKey)
-					gql := `
+		var subscribedPostIDs = make(map[string]interface{})
+		var hasPremiumPrivilege bool
+		if tokenState == token.OK {
+			firebaseID := c.GetString(middleware.GCtxUserIDKey)
+			gql := `
 query ($firebaseId: String!) {
   member(where: {firebaseId: $firebaseId}) {
     subscription(where: {isActive: true}) {
@@ -110,64 +79,78 @@ query ($firebaseId: String!) {
   }
 }
 `
-					req := graphqlclient.NewRequest(gql)
-					req.Var("firebaseId", firebaseID)
-					data := model.Member{}
+			req := graphqlclient.NewRequest(gql)
+			req.Var("firebaseId", firebaseID)
+			// data := model.Member{}
+			member := struct {
+				Member model.Member `json:"member"`
+			}{}
 
-					var ids = make(map[string]interface{})
-					var privilege bool
-					var err error
+			client := graphql.NewClient(memberGraphqlEndpoint, graphql.WithHTTPClient(httpclient.DefaultNetHttpClient))
+			err = client.Run(context.TODO(), req, &member)
+			if err != nil {
+				logger.Errorf("cannot fetch member and subscription state from member server")
+				c.AbortWithStatusJSON(http.StatusInternalServerError, Reply{
+					TokenState: tokenState,
+				})
+				return
+			}
 
-					client := graphql.NewClient(memberGraphqlEndpoint, graphql.WithHTTPClient(httpclient.DefaultNetHttpClient))
-					err = client.Run(context.TODO(), req, &data)
-					// defer sending values to chanels to make sure that every value is properly assigned in the workflow and the channel won't be block receiving
-					defer func() {
-						errChan <- err
-						subscribedPostIDs <- ids
-						premiumPrivilege <- privilege
-					}()
-					if err != nil {
-						return
-					}
-
-					if data.Subscription != nil {
-						for _, s := range data.Subscription {
-							if *s.Frequency == model.SubscriptionFrequencyTypeOneTime {
-								ids[*s.PostID] = nil
-							} else {
-								privilege = true
-								break
-							}
-						}
+			data := member.Member
+			if data.Subscription != nil {
+				for _, s := range data.Subscription {
+					if *s.Frequency == model.SubscriptionFrequencyTypeOneTime {
+						subscribedPostIDs[*s.PostID] = nil
+					} else {
+						hasPremiumPrivilege = true
+						break
 					}
 				}
-			}()
+			}
+		}
+
+		var body []byte
+		redisKey := fmt.Sprintf("%s.%s.%s.%s", "apigateway", "proxy", "uri", c.Request.RequestURI)
+		if cmd := rdb.Get(context.TODO(), redisKey); cmd == nil {
+			// cache doesn't exist, do fetch reverse proxy
+			logger.Infof("cache for uri(%s) cannot be fetched", c.Request.RequestURI)
+		} else if body, err = cmd.Bytes(); err != nil {
+			// cache can't be understood, do fetch reverse proxy
+			logger.Warnf("cache for uri(%s) cannot be converted to bytes", c.Request.RequestURI)
+		} else {
+			switch path := c.Request.URL.Path; {
+			case strings.HasSuffix(path, "/getposts") || strings.HasSuffix(path, "/posts") || strings.HasSuffix(path, "/post"):
+				// break the switch to continue with response from proxied request
+				var itemsLength int
+				if itemsLength, body, err = modifyPostItems(logger, body, subscribedPostIDs, hasPremiumPrivilege); err != nil {
+					logger.Warnf("modifyPostItems in cache encounter error: %s", err)
+					break
+				}
+
+				if body, err = removePodtItemsHtml(body, itemsLength); err != nil {
+					logger.Warnf("encounter error when deleting html in cache:", err)
+					break
+				}
+				c.Header("GW-Cache", time.Now().Format(time.RFC3339))
+				c.AbortWithStatusJSON(http.StatusOK, Reply{
+					TokenState: tokenState,
+					Data:       json.RawMessage(body),
+				})
+				return
+			}
 		}
 
 		reverseProxy := httputil.ReverseProxy{Director: director}
-		reverseProxy.ModifyResponse = ModifyReverseProxyResponse(c, rdb, cacheTTL, subscribedPostIDs, premiumPrivilege, errChan)
+		reverseProxy.ModifyResponse = ModifyReverseProxyResponse(c, rdb, cacheTTL, tokenState, subscribedPostIDs, hasPremiumPrivilege)
 		reverseProxy.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
-func ModifyReverseProxyResponse(c *gin.Context, rdb cache.Rediser, cacheTTL int, subscribedPostIDs chan map[string]interface{}, premiumPrivilege chan bool, errChan chan error) func(*http.Response) error {
+func ModifyReverseProxyResponse(c *gin.Context, rdb cache.Rediser, cacheTTL int, tokenState string, subscribedPostIDs map[string]interface{}, hasPremiumPrivilege bool) func(*http.Response) error {
 	logger := logrus.WithFields(logrus.Fields{
 		"path": c.FullPath(),
 	})
 	return func(r *http.Response) error {
-		// check error first for short circuit
-		select {
-		case err := <-errChan:
-			if err != nil {
-				logger.Error(err)
-				return err
-			}
-		case <-time.After(1 * time.Second):
-			err := fmt.Errorf("timeout for one seconds for getting %s", "error from member subscription fetching")
-			logger.Error(err)
-			return err
-		}
-
 		body, err := io.ReadAll(r.Body)
 		_ = r.Body.Close()
 		if err != nil {
@@ -175,123 +158,31 @@ func ModifyReverseProxyResponse(c *gin.Context, rdb cache.Rediser, cacheTTL int,
 			return err
 		}
 
-		var tokenState string
+		// Save the complete post as early as we can and run in in a goroutine
+		redisKey := fmt.Sprintf("%s.%s.%s.%s", "apigateway", "proxy", "uri", c.Request.RequestURI)
+		go func(rdb cache.Rediser, body []byte, redisKey string) {
+			if err = rdb.Set(context.TODO(), redisKey, body, time.Duration(cacheTTL)*time.Second).Err(); err != nil {
+				logger.Warnf("setting redis cache(%s) encountered error: %v", redisKey, err)
+			}
+		}(rdb, body, redisKey)
 
-		tokenSaved, exist := c.Get(middleware.GCtxTokenKey)
-		if !exist {
-			tokenState = "No Bearer token available"
-		} else {
-			tokenState = tokenSaved.(token.Token).GetTokenState()
-		}
-
-		var redisKey string
 		switch path := r.Request.URL.Path; {
 		// TODO refactor condition
 		case strings.HasSuffix(path, "/getposts") || strings.HasSuffix(path, "/posts") || strings.HasSuffix(path, "/post"):
 
-			type Category struct {
-				IsMemberOnly *bool `json:"isMemberOnly,omitempty"`
-			}
-
-			type ItemContent struct {
-				APIData []interface{} `json:"apiData"`
-			}
-			type Item struct {
-				ID         string      `json:"_id"`
-				Content    ItemContent `json:"content"`
-				Categories []Category  `json:"categories"`
-			}
-			type Resp struct {
-				Items []Item `json:"_items"`
-			}
-
-			var items Resp
-			err = json.Unmarshal(body, &items)
-			if err != nil {
-				logger.Errorf("Unmarshal post encountered error: %v", err)
-				return err
-			}
-
-			// truncate the content if the user is not a member and the post falls into a member only category
-
 			// modify body if the item falls into a "member only" category
-			var subscribedIds map[string]interface{}
-			var hasPremiumPrivilege bool
 
-			select {
-			case subscribedIds = <-subscribedPostIDs:
-			case <-time.After(1 * time.Second):
-				err = fmt.Errorf("timeout for one seconds for getting %s", "subscribedPostIDs")
-				logger.Error(err)
+			var itemsLength int
+			if itemsLength, body, err = modifyPostItems(logger, body, subscribedPostIDs, hasPremiumPrivilege); err != nil {
+				logger.Errorf("modifyPostItems encounter error: %s", err)
 				return err
 			}
 
-			select {
-			case hasPremiumPrivilege = <-premiumPrivilege:
-			case <-time.After(1 * time.Second):
-				err = fmt.Errorf("timeout for one seconds for getting %s", "premiumPrivilege")
-				logger.Error(err)
+			if body, err = removePodtItemsHtml(body, itemsLength); err != nil {
+				logger.Errorf("encounter error when deleting html:", err)
 				return err
 			}
 
-			toTruncateIt := func(category Category, postID string) bool {
-				if category.IsMemberOnly == nil || !*category.IsMemberOnly {
-					return true
-				}
-				_, postSubscribed := subscribedIds[postID]
-				return !hasPremiumPrivilege && !postSubscribed
-			}
-
-			var truncated bool
-
-			for i, item := range items.Items {
-				for _, category := range item.Categories {
-					if toTruncateIt(category, item.ID) {
-						truncatedAPIData := item.Content.APIData[0:3]
-						body, err = sjson.SetBytes(body, fmt.Sprintf("_items.%d.content.apiData", i), truncatedAPIData)
-						if err != nil {
-							logger.Errorf("encounter error when truncating apiData:", err)
-							return err
-						}
-						truncated = true
-						body, err = sjson.SetBytes(body, fmt.Sprintf("_items.%d.isTruncated", i), true)
-						if err != nil {
-							logger.Errorf("encounter error setting isTruncated to true for _items.%d", i, err)
-							return err
-						}
-						break
-					} else {
-						body, err = sjson.SetBytes(body, fmt.Sprintf("_items.%d.isTruncated", i), false)
-						if err != nil {
-							logger.Errorf("encounter error setting isTruncated to false for _items.%d", i, err)
-							return err
-						}
-					}
-				}
-			}
-
-			// remove html because only apidata is useful and html contains full content
-			for i, _ := range items.Items {
-				body, err = sjson.DeleteBytes(body, fmt.Sprintf("_items.%d.content.html", i))
-				if err != nil {
-					logger.Errorf("encounter error when deleting html:", err)
-					return err
-				}
-			}
-
-			// FIXME it's not a single condition anymore!
-			if truncated {
-				// TODO refactor redis cache code
-				redisKey = fmt.Sprintf("%s.%s.%s.%s", "apigateway", "post", "truncated", c.Request.RequestURI)
-			} else {
-				// TODO refactor redis cache code
-				redisKey = fmt.Sprintf("%s.%s.%s.%s", "apigateway", "post", "clean", c.Request.RequestURI)
-			}
-			// TODO refactor redis cache code
-			err = rdb.Set(context.TODO(), redisKey, body, time.Duration(cacheTTL)*time.Second).Err()
-			if err != nil {
-				logger.Warnf("setting redis cache(%s) encountered error: %v", redisKey, err)
-			}
 		default:
 		}
 
@@ -310,6 +201,74 @@ func ModifyReverseProxyResponse(c *gin.Context, rdb cache.Rediser, cacheTTL int,
 		r.Header.Set("Content-Length", strconv.Itoa(len(b)))
 		return nil
 	}
+}
+
+func modifyPostItems(logger *logrus.Entry, body []byte, subscribedPostIDs map[string]interface{}, hasPremiumPrivilege bool) (postItemsLength int, modifiedBody []byte, err error) {
+	type Category struct {
+		IsMemberOnly *bool `json:"isMemberOnly,omitempty"`
+	}
+
+	type ItemContent struct {
+		APIData []interface{} `json:"apiData"`
+	}
+	type Item struct {
+		ID         string      `json:"_id"`
+		Content    ItemContent `json:"content"`
+		Categories []Category  `json:"categories"`
+	}
+	type Resp struct {
+		Items []Item `json:"_items"`
+	}
+
+	var items Resp
+	err = json.Unmarshal(body, &items)
+	if err != nil {
+		err = fmt.Errorf("unmarshal post body encountered error: %v", err)
+		return 0, nil, err
+	}
+
+	// modify body at the end and truncate the post depending on the post and member state
+	for i, item := range items.Items {
+		for _, category := range item.Categories {
+			isPostPremium := category.IsMemberOnly != nil && *category.IsMemberOnly
+			if isPostPremium && isPostToBeTruncate(isPostPremium, item.ID, hasPremiumPrivilege, subscribedPostIDs) {
+				truncatedAPIData := item.Content.APIData[0:3]
+				body, err = sjson.SetBytes(body, fmt.Sprintf("_items.%d.content.apiData", i), truncatedAPIData)
+				if err != nil {
+					err = fmt.Errorf("encounter error when truncating apiData: %v", err)
+					return 0, nil, err
+				}
+				body, err = sjson.SetBytes(body, fmt.Sprintf("_items.%d.isTruncated", i), true)
+				if err != nil {
+					err = fmt.Errorf("encounter error setting isTruncated to true for _items.%d: %v", i, err)
+					return 0, nil, err
+				}
+				break
+			} else {
+				body, err = sjson.SetBytes(body, fmt.Sprintf("_items.%d.isTruncated", i), false)
+				if err != nil {
+					err = fmt.Errorf("encounter error setting isTruncated to false for _items.%d: %v", i, err)
+					return 0, nil, err
+				}
+			}
+		}
+	}
+	return len(items.Items), body, err
+}
+
+func removePodtItemsHtml(body []byte, itemsLength int) (modifiedBody []byte, err error) {
+	for i := 0; i < itemsLength-1; i++ {
+		body, err = sjson.DeleteBytes(body, fmt.Sprintf("_items.%d.content.html", i))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return body, err
+}
+
+func isPostToBeTruncate(isPostPremium bool, postID string, hasPremiumPrivilege bool, subscribedIds map[string]interface{}) bool {
+	_, postSubscribed := subscribedIds[postID]
+	return isPostPremium && !hasPremiumPrivilege && !postSubscribed
 }
 
 func joinURLPath(a, b *url.URL) (path, rawpath string) {
